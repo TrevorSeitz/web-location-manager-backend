4105da165984f4311204442b9b9c91eed8dcc40b
TrevorSeitz <trevinla@gmail.com>
2018-10-03 11:22:29 -0400

    set update method

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -15,8 +15,13 @@ class PlacesController < ApplicationController
 # @place.image.attach(io: File.open(params[:image]), filename: params[:fileName], content_type: 'image/jpeg', identify: false)
   end
 
+  def edit
+  end
+
   def update
     byebug
+    @place = Place.find(place_params[:id])
+    @place.update(name: place_params[:name], venue: place_params[:venue], contactName: place_params[:contactName], contactPhone: place_params[:contactPhone], email: place_params[:email], description: place_params[:description])
   end
 
 
@@ -24,7 +29,7 @@ class PlacesController < ApplicationController
   private
 
   def place_params
-    params.require(:place).permit(:name, :fileName, :venue, :longitude, :latitude, :contactName, :contactPhone, :email, :permit, :description, :GPSLatitudeRef, :GPSLongitudeRef, :image)
+    params.require(:place).permit(:id, :name, :fileName, :venue, :longitude, :latitude, :contactName, :contactPhone, :email, :permit, :description, :GPSLatitudeRef, :GPSLongitudeRef, :image)
   end
 
   def search_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'places#index'
   post 'uploader/image', to: 'uploader#image'
+  # patch 'api/places/:id', to: 'places#update'
 
   scope :api do
     # resources :places, only: [:index, :new, :create]
-    resources :places, :photos
+    resources :places, :images
   end
 
 end
