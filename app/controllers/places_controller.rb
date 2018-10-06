371b54a5c72bd345fe914dca317d92ba1a7ae2e7
TrevorSeitz <trevinla@gmail.com>
2018-10-06 12:42:25 -0400

    delete from edit page working

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,29 +1,35 @@
 class PlacesController < ApplicationController
   def index
     # byebug
-    places = Place.search(search_params.to_h.symbolize_keys)
+    @places = Place.search(search_params.to_h.symbolize_keys)
+    # places = Place.search(search_params.to_h.symbolize_keys).with_attached_image
       # byebug
-    render json: places
-
+    render json: @places
+    # render json: places.to_json(include: { image_attachment: { include: :blob } })
+    # render json: @places, include: [{document: {include: {files: {include: {attachments: {include: {blob: {methods: :service_url}}}}} }}]
   end
 
   def create
     @place = Place.create(place_params)
     @place.image.attach(params[:image])
     @place.save
-
-# @place.image.attach(io: File.open(params[:image]), filename: params[:fileName], content_type: 'image/jpeg', identify: false)
   end
 
   def edit
   end
 
+
+  def destroy
+    # debugger
+    @place = Place.find(params[:id])
+    @place.destroy
+  end
+
   def update
-    byebug
+    debugger
     @place = Place.find(place_params[:id])
     @place.update(name: place_params[:name], venue: place_params[:venue], contactName: place_params[:contactName], contactPhone: place_params[:contactPhone], email: place_params[:email], description: place_params[:description])
   end
-
 
 
   private
@@ -36,3 +42,5 @@ class PlacesController < ApplicationController
     params.permit(:min_lng, :max_lng, :min_lat, :max_lat, :image)
   end
 end
+
+# google api key: AIzaSyCUK5vir8VCLXEU3DR_BDWU0unPxnEz7lE
